b77520ebdc0c3afb4c95ce292068ea68fd007124
Myr <75548109+myrmaidx@users.noreply.github.com>
2022-03-16 15:33:10 +0100

    kepler updates

--- js/main.js
+++ js/main.js
@@ -88,7 +88,7 @@
             const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
             // Fly the camera to San Francisco at the given longitude, latitude, and height.
             viewer.camera.flyTo({
-            destination : Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+            destination : Cesium.Cartesian3.fromDegrees(4.895167899999933, 52.3702157, 400),
             orientation : {
                 heading : Cesium.Math.toRadians(0.0),
                 pitch : Cesium.Math.toRadians(-15.0),
