e5019f631b7b2432043d873c9905c68b7f48198a
Myr <75548109+myrmaidx@users.noreply.github.com>
2022-03-29 13:50:54 +0200

    i fixed my soup

--- js/main.js
+++ js/main.js
@@ -77,26 +77,7 @@
 
         // END RDNEWMAP
 
-            // NO3 = MAP CESIUM !!!
-            Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzMjM4YWQ0YS1jNDIzLTQ0ZGUtYmRlOS00ZGQwOGIzOWU5ZTkiLCJpZCI6ODUwODIsImlhdCI6MTY0Njc2ODE4OH0.qiGlaAkcSKH_l7gK5WL0-uC1D7ID5CZwDs93syqGc3M';
-
-            // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-            const viewer = new Cesium.Viewer('cesiumContainer', {
-            terrainProvider: Cesium.createWorldTerrain()
-            });    
-            // Add Cesium OSM Buildings, a global 3D buildings layer.
-            const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
-            // Fly the camera to San Francisco at the given longitude, latitude, and height.
-            viewer.camera.flyTo({
-            destination : Cesium.Cartesian3.fromDegrees(4.895167899999933, 52.3702157, 400),
-            orientation : {
-                heading : Cesium.Math.toRadians(0.0),
-                pitch : Cesium.Math.toRadians(-15.0),
-            }
-            });
-
-            // END MAP CESIUM
-
+        
         // BUTTON, CORRELATION WITH RDNEWMAP NO2
 
         const geoJsonlayerHHW = L.geoJSON().addTo(RDNewMap);
---
+++ js/project.js
@@ -0,0 +1,19 @@
+// NO3 = MAP CESIUM !!!
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIzMjM4YWQ0YS1jNDIzLTQ0ZGUtYmRlOS00ZGQwOGIzOWU5ZTkiLCJpZCI6ODUwODIsImlhdCI6MTY0Njc2ODE4OH0.qiGlaAkcSKH_l7gK5WL0-uC1D7ID5CZwDs93syqGc3M';
+
+// Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
+const viewer = new Cesium.Viewer('cesiumContainer', {
+terrainProvider: Cesium.createWorldTerrain()
+});    
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+destination : Cesium.Cartesian3.fromDegrees(4.895167899999933, 52.3702157, 400),
+orientation : {
+    heading : Cesium.Math.toRadians(0.0),
+    pitch : Cesium.Math.toRadians(-15.0),
+}
+});
+
+// END MAP CESIUM
